3cc2a3affb6a041b168df4a8b1c2b31635264fe0
Qihang Feng <83911295+Deepdive543443@users.noreply.github.com>
2023-05-13 16:20:21 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,30 +1,6 @@
 from lightning.pytorch.callbacks import Callback, ProgressBar, TQDMProgressBar
 # https://lightning.ai/docs/pytorch/stable/extensions/callbacks.html
-import json, os, sys
-from torchtext.data.utils import get_tokenizer
-from tensorboard import program
-output_stream = sys.stdout
 
-en_tokenizer = get_tokenizer('spacy', language='en')
-de_tokenizer = get_tokenizer('spacy', language='de')
-
-class print_example_callback(Callback):
-    def __init__(self, trainset, testset):
-        super(print_example_callback, self).__init__()
-        self.trainset = trainset
-        self.testset = testset
-
-
-    def on_train_epoch_end(self, trainer, pl_module):
-        print('\nTraining result')
-        input_pair, target_pair = self.trainset.random_pairs()
-        translated_sentences, bleu_score = pl_module.translate(input_pair, target_pair)
-        print(f"Input: {input_pair}\nTarget: {target_pair}\nTranslate: {translated_sentences}\nBLEU: {bleu_score}\n")
-
-        print('Testing result')
-        input_pair, target_pair = self.testset.random_pairs()
-        translated_sentences, bleu_score = pl_module.translate(input_pair, target_pair)
-        print(f"Input: {input_pair}\nTarget: {target_pair}\nTranslate: {translated_sentences}\nBLEU: {bleu_score}\n")
 
 class KstyleBar(ProgressBar):
     def __init__(self, bar_length, args, train_stat = ['v_num'], val_stat = ['v_num'], test_stat = ['v_num']):
@@ -105,63 +81,3 @@ class KstyleBar(ProgressBar):
                 None
 
         print("\r" +'Evaluating... ' + percent_info + bar + batch_idx_info + loss_info, end='\r', flush=True)
-
-
-# class MyProgressBar(TQDMProgressBar):
-#     def init_validation_tqdm(self):
-#         bar = super().init_validation_tqdm()
-#         if not sys.stdout.isatty():
-#             bar.disable = True
-#         return bar
-#
-#     def init_predict_tqdm(self):
-#         bar = super().init_predict_tqdm()
-#         if not sys.stdout.isatty():
-#             bar.disable = True
-#         return bar
-#
-#     def init_test_tqdm(self):
-#         bar = super().init_test_tqdm()
-#         if not sys.stdout.isatty():
-#             bar.disable = True
-#         return bar
-
-
-def launch_tensorboard(tracking_address):
-    # https://stackoverflow.com/a/55708102
-    # tb will run in background but it will
-    # be stopped once the main process is stopped.
-    try:
-        tb = program.TensorBoard()
-        tb.configure(argv=[None, '--logdir', tracking_address, '--port', '8008'])
-        url = tb.launch()
-        if url.endswith("/"):
-            url = url[:-1]
-
-        return url
-    except Exception:
-        return None
-
-
-
-
-def beam_search():
-    pass
-
-
-def save_config_json(args, path):
-    with open(os.path.join(path, 'config.json'), "w") as outfile:
-        json.dump(args, outfile)
-
-def load_config_json(path):
-    with open(path, 'r') as infile:
-        obj = json.load(infile)
-    return obj
-
-if __name__ == "__main__":
-    for i in range(0, 999999):
-        percent = ( i / 999999) * 100
-        print(f'\r{percent:.01f}', end='\r', flush=True)
-    print(f'{100:.01f} percent complete')
-        # output_stream.write(f'{percent:.01f} percent complete', )
-        # output_stream.flush()
